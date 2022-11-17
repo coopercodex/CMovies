5b3bac091636b4129ac9562a7937f3dae9a4ed4b
Derek Cooper <derekcooper20@gmail.com>
2022-11-17 13:48:39 -0700

    Adds styling and navbar

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -15,29 +15,27 @@ export const Featured = () => {
       .then(data => { setMovies(data.results[Math.floor(Math.random() * data.results.length - 1)]) })
       .catch(error => { console.log(error) })
   }
-  console.log(movies)
+  // console.log(movies)
   return (
     <header className='featured'
       style={{
         backgroundSize: 'cover',
         backgroundImage: `url(
         "https://image.tmdb.org/t/p/original/${movies?.backdrop_path}")`, backgroundPosition: "center center",
-      }}
-
-    >
+      }}>
       <div className='featured-movies'>
-        <h1>
+        <h1 className='featured-title'>
           {movies?.title || movies?.name || movies?.original_name}
         </h1>
-        <div className='featured-button'>
+      <div className="featured-buttons">
           <button className="featured-button">Play</button>
           <button className="featured-button">My List</button>
-        </div>
+      </div>
         <h1 className='featured-description'>
           {movies.overview}
         </h1>
-
       </div>
+      <div className='fade-bottom'/>
 
     </header>
   )
