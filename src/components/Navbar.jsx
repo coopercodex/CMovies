c8332f3e97a5e24b7972aa81bc17212067b62775
Derek Cooper <derekcooper20@gmail.com>
2022-11-17 15:09:43 -0700

    Refactors styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Navbar = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        handleShow(true); 
+      } else {
+        handleShow(false)
+      }
+    })
+    return () => {
+      window.removeEventListener('scroll')
+    }
+  },[])
+
   return (
-    <div className='navbar'>
-      <h1 className='navbar-title'>C Movies</h1>
+    <div className={`navbar ${show && 'navbar-black'}`}>
+      <h1 className='navbar-title'>CMovies</h1>
     </div>
   )
 }
