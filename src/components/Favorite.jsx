43cf4a3fc228806338b02f0749e162a6cd04e2df
Derek Cooper <derekcooper20@gmail.com>
2022-11-18 17:05:23 -0700

    Refactor favorites

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -3,11 +3,16 @@ import YouTube from 'react-youtube'
 
 export const Favorite = () => {
   return (
+    <>
   <div className='fav'>
-    <div>
-   <h1 className='fav-title'>Favorites!</h1>
+    <div className='fav-title'>
+   <h1>Favorites!</h1>
+   <img src='https://thumbs.dreamstime.com/b/under-construction-pink-pink-under-construction-sign-206884845.jpg'/>
+   <YouTube 
+    videoId={'BokDch3_g-Y'}
+   />
     </div>
   </div>
- 
+  </>
   )
 }
