52c2a664b190eecd1cdf626b9be23431b51ab5ca
Derek Cooper <derekcooper20@gmail.com>
2023-01-26 01:55:00 -0700

    Styles

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,7 +5,8 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword  } 
 function SignIn() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
-  
+  const email = 'cmovies@gmail.com'
+  const password = 'cmovies123'
   const register = (event) => {
     event.preventDefault();
     const auth = getAuth();
@@ -32,16 +33,16 @@ function SignIn() {
     <div className='signIn-screen'>
       <form>
         <h1>Sign In</h1>
-        <input ref={emailRef} placeholder='abc@gmail.com' type='email' />
-        <input ref={passwordRef} placeholder='abc123' type='password' />
+        <input ref={emailRef} placeholder='Email' type='email' value={email} />
+        <input ref={passwordRef} placeholder='Password' type='password' value={password} />
         <button type='submit' onClick={signIn}>Sign In</button>
         <h4>
           <span className='signIn-gray'>New to CmMovies? </span>
           <span className='signIn-link' onClick={register}>Sign Up Now</span> 
-          <h3 className='message' >(Use sign in inside the input field or any fake gmail and password will work!)</h3>
+          <h3 className='message' >(Just click Sign In! Click sign up to see Firebase...well fire!)</h3>
         </h4>
       </form>
-    </div>
+      </div>
   )
 }
 
